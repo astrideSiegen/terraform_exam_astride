46f7f059f00d6207b806d6e4189e7f67376b47d5
Daniel <daniel@datascientest.com>
2025-03-14 21:41:56 +0000

    terraform files created also shell file

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -3,6 +3,7 @@ variable "namespace" {
   type    = string
   default = "wordpress"
 }
+
 #intance type
 variable "instance_type" {
   description = "type d'intance ec2"
@@ -21,6 +22,28 @@ variable "public_subnet_ids" {
 variable "private_subnet_ids" {
   description = "Liste des subnets privés"
   type        = list(string)
+}
+
+# valeurs de rds à ajouter sur notre ec2 lors du deploiement
+variable "database_name" {
+  description = "Nom de la base de données RDS"
+  type        = string
+}
+
+variable "database_user" {
+  description = "Nom d'utilisateur de la base de données RDS"
+  type        = string
+}
+
+variable "database_password" {
+  description = "Mot de passe de la base de données RDS"
+  type        = string
+  sensitive   = true
+}
+
+variable "database_host" {
+  description = "Adresse de la base de données RDS"
+  type        = string
 }
 
 # variable "security_group_id" {
